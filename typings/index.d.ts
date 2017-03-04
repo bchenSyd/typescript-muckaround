4bb324ee5b9ade67d1097887c9334db05cbfb787
Bo Chen <bochen2014@yahoo.com>
2017-03-04 14:26:55 +1100

    add typings support

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/react-redux/index.d.ts" />
+/// <reference path="modules/react/index.d.ts" />
+/// <reference path="modules/redux/index.d.ts" />
