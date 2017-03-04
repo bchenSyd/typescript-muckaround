52f192d53617b7211e023cb387e690b4406f3834
Bo Chen <bochen2014@yahoo.com>
2017-03-04 15:15:26 +1100

    it was actually working

--- ts/react/MyComponent.tsx
+++ ts/react/MyComponent.tsx
@@ -1,18 +1,22 @@
-import * as React from 'react'
-import { Component } from 'react';
+import * as React from 'react' //tslint error
+import { Component } from 'react'; 
 
 interface IMyComponentProps {
-    name: string,
+    name: string;
     contact_methods: string[]
 }
 
 class MyComponent extends Component<IMyComponentProps, any> {
     render() {
         const { name } = this.props
+        if (true) {
+            console.log('test') //tslint error
+        }
         //const {none_exists} = this.props  // typescript error
         return (
             <div>
-
+                <div></div>
+                <button onClick={()=>{alert('go')}} />
             </div>
         );
     }
