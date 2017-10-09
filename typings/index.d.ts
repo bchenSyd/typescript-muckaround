f845daa4ad4977e458209f6eaf8a1a0ee5f7843b
Bo Chen <bochen2014@yahoo.com>
2017-10-10 00:03:28 +1100

    update

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,7 @@
-/// <reference path="modules/react-redux/index.d.ts" />
+// https://stackoverflow.com/a/28991341/6560291
+// seems not needed after typescript v1.5
+// comment below line out, IMapStateToProps can still be resolved; compile is also OK;
+/*    /// <reference path="modules/react-redux/index.d.ts" />    */
+
+
 /// <reference path="modules/redux/index.d.ts" />
