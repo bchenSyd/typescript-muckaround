215183047bf566517c0dc3cf4d5f2049a37124e4
Bo Chen <bo.chen@kindredgroup.com>
2017-10-10 15:49:56 +1100

    figure out the reason why ///<reference path='xyz'/> isn't required

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,5 @@
 // https://stackoverflow.com/a/28991341/6560291
-// seems not needed after typescript v1.5
-// comment below line out, IMapStateToProps can still be resolved; compile is also OK;
+// right... the `/// <reference>` tags is deprectead in favour of a tsconfig.json file
 /*    /// <reference path="modules/react-redux/index.d.ts" />    */
 
 
