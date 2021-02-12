b4aab7be3523b832a01462778f42d0c40bcf6f92
Bo Chen <bochen2014@yahoo.com>
2021-02-12 15:54:42 +1100

    update

--- src/class/myClass.ts
+++ src/class/myClass.ts
@@ -1,12 +1,4 @@
-/// <reference path="global.d.ts" />
-/**
- * global.d.ts doesn't export, to reference, use ///
- * 
- * 
- */
-export interface IMyInterface {
-  name:string
-}
-export class MyClass implements IPros {
-  name: "name";
-}
+export const ross: Person = {
+  name: "bo chen",
+  age: 39,
+};
