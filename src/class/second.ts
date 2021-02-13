f8f513903f3f95ee3f451023b60db2a65d1eaf7e
Bo Chen <bochen2014@yahoo.com>
2021-02-13 15:57:31 +1100

    feat: no-undef

--- src/class/second.ts
+++ src/class/second.ts
@@ -1,7 +1,7 @@
 const yakka: Fish = {
   swim() {
     // https://github.com/typescript-eslint/typescript-eslint/issues/1856
-    console.log(abc) // scopeManager issue; eslint-global, @typescript-eslint doesn't want to 
+    console.log(abc) // scopeManager issue; `no-undef`, @typescript-eslint doesn't want to 
     // reinvent the wheels while compile is already handling it;
     console.log("i can swim!");
   },
