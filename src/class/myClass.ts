2318e526ec6ae1a1f6cf6ba7d76058af1d277324
Bo Chen <bochen2014@yahoo.com>
2021-02-14 20:17:23 +1100

    const assertions

--- src/class/myClass.ts
+++ src/class/myClass.ts
@@ -20,3 +20,12 @@ type setStateFunction<T> = (value: T) => T;
 // declare const TestFunction: setStateFunction<number>;
 const TestFunction: setStateFunction<number> = (val) => val + 1;
 TestFunction(1);
+
+function MyFunction(input: string) {
+  const length = input.length;
+
+  // const assertions
+  // you will want to avoid type inference as TypeScript will infer a union type 
+  //(when you actually want different types in each position of the array). Instead, use TS 3.4 const assertions
+  return [input, length] as const; // infers [string, number] instead of (string|number)[]
+}
