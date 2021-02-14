23fc6098b585571492b599f35bb5be8285c11a77
Bo Chen <bochen2014@yahoo.com>
2021-02-14 16:11:17 +1100

    update

--- src/class/myClass.ts
+++ src/class/myClass.ts
@@ -1,9 +1,19 @@
+import { Agent } from "http";
+
 interface Fish {
-  swim();
+  swim():number;
 }
 
 
+// d.ts output: declare const ross: Person;
 const ross: Person = {
-  name: "bo chen",
+  name: "Ross S",
   age: 39,
 };
+
+// once you have `export` keywork, you convert you currnet file from "script" to "module"
+// and Fish won't be visible globally
+// export const Gareth: Person ={
+//   name: "Gareth J",
+//   age:40
+// }
--- src/class/second.ts
+++ src/class/second.ts
@@ -4,5 +4,6 @@ const yakka: Fish = {
     console.log(abc) // scopeManager issue; `no-undef`, @typescript-eslint doesn't want to 
     // reinvent the wheels while compile is already handling it;
     console.log("i can swim!");
+    return 1;
   },
 };
