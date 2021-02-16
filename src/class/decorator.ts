9266c3d1f2457ed6cef4dfecac7aab210c020e85
Bo Chen <bochen2014@yahoo.com>
2021-02-16 21:50:22 +1100

    update

--- src/class/decorator.ts
+++ src/class/decorator.ts
@@ -1,21 +1,24 @@
+import { Component } from "react";
+
   export function DispatchContextDecoratorFactory(): ClassDecorator {
       return (Component) => {
+          // NOTE 1: not returnning value; modify constructor
+          
           Component.contextTypes = Component.contextTypes || {};
           Component.contextTypes.dispatch = React.PropTypes.func.isRequired;
       };
   }
   
-  // TODO async dispatch returns Promise
-  // Fix to include promise in signature
-  export interface IDispatchContext {
-      dispatch: ICommon.IReduxStoreDispatch;
-  }
+
   
-  
-  //********************************************* */
+ // https://stackoverflow.com/a/54249355/6560291
+ // I also am curious why the React community calls this Higher-Order Component (HOC) 
+ // instead of Decorator pattern, which seems to be what it exactly is doing and has been described in 1994 (!) 
+ // by the Gang of Four (GoF): Erich Gamma, Richard Helm, Ralph Johnson, and John Vlissides.
   
   export function HistoryListenerFactory<P>(): ClassDecorator {
       return (Component): any => {
+          // NOTE2: returnning a value; this is the same as react HOC
           class HistoryListenerComponent extends React.Component<any, any> {
   
               static contextTypes = {
@@ -27,7 +30,13 @@
               };
   
   
-  
+             render(){
+                 return <Component {...this.props} />
+             }
+            }
+            return HistoryListenerComponent
+        }
+    }
   
   
   
